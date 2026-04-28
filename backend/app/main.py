06a4c8962c3762b2593e3e1f8973ee0fe3f87684
Glenn <alertislow@gmail.com>
2026-04-28 18:46:41 +0800

    fix(cors): update allowed origins in CORSMiddleware configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "https://fitness-app-drab-nine.vercel.app"
     ],
+    allow_origin_regex=r"https://fitness-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
